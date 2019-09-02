a7b6f0b09e744e0b365bb0882ac5572613710972
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-09-02 18:32:23 +0200

    add a button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    count: 0,
+  };
+
   render() {
-    // debugger;
-    return <h1>Hello world</h1>;
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <h2>Count: ${this.state.count}</h2>
+        <button
+          onClick={() => this.setState(state => ({ count: state.count }))}
+        >
+          Click Me!
+        </button>
+      </div>
+    );
   }
 }
 
