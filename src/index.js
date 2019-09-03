472954f616811515f3cebb05af9fcc48a4f5220d
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-09-03 16:19:56 +0200

    this has added a lot of code, but we can target browsers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// import '@babel/polyfill'
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 import App from './App';
 
 import './styles.css';
