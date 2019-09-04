9a770ebb85e0fa5c407603050b06d8147f6c87b2
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-09-04 18:19:12 +0200

    paring down the app so we can clone it

--- src/App.js
+++ src/App.js
@@ -1,46 +1,14 @@
-import React, { Component, Suspense } from 'react'
+import React, { Component } from 'react'
 import { hot } from 'react-hot-loader'
 
-const Warning = React.lazy(() => import('./Warning'))
-
 class App extends Component {
-  state = {
-    count: 0,
-  }
-
-  increment = () => {
-    this.setState(state => ({ count: state.count + 1 }))
-  }
-
-  decrement = () => {
-    this.setState(state => ({ count: state.count - 1 }))
-  }
-
   render() {
-    // throw new Error('Boom!')
-
-    const { count } = this.state
-
     return (
       <div>
         <h1>Hello World!</h1>
-
-        <h2 className={count > 10 ? 'warning' : null}>Count: {count}</h2>
-
-        <button onClick={this.increment}>+</button>
-        <button onClick={this.decrement}>-</button>
-
-        {count > 10 ? (
-          <Suspense fallback={null}>
-            <Warning />
-          </Suspense>
-        ) : null}
       </div>
     )
   }
 }
-
-// const hotFunction = hot(module)
-// export default hotFunction(App);
 
 export default hot(module)(App)
